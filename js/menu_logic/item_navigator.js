1aa723ea778f8125eaf9e3f37a7b38c1d4e9c12a
Andy Poquis <andrespoquis@gmail.com>
2021-10-19 13:33:23 -0500

    cambios importantes

--- js/menu_logic/item_navigator.js
+++ js/menu_logic/item_navigator.js
@@ -2,3 +2,8 @@ $('#contac').click(function () {
     
     window.location = '../../pages/contac.html'
 });
+
+$('#category').click(function () { 
+    
+    window.location = '../../pages/categoria.html'
+});
